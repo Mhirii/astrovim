ce82f619e283287ebc7d688f64f520f52342f171
Mhirii <ahmedmhiri218@gmail.com>
2024-06-02 05:44:51 +0100

    diable hiding dotfiles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,5 +4,19 @@ return {
     window = {
       position = "right",
     },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true,
+        hide_by_name = {
+          "node_modules",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          ".env",
+          "wrangler.toml",
+        },
+      },
+    },
   },
 }
